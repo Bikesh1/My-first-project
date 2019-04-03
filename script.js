e190a034e2c9f046371959e70946c4231503d8d7
Bikesh1 <48976394+Bikesh1@users.noreply.github.com>
2019-04-03 13:38:24 +1000

    made script file

---
+++ script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "In an interview shortly after Gamescom, Greene said that their deal with Microsoft did not exclude a PlayStation 4 port, but that their focus at the time was only on the Windows and Xbox One version, given the small size of their team.";
+}
